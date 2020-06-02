115ac109e7ac496fc649bc19f06cba6874d83d37
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-02 14:09:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,9 @@ class Song
   end
 
   def self.create
-    self.new.save
-    #song = self.new
-    #song.save
-    #ong
+    song = self.new
+    song.save
+    song
   end
 
   def self.new_by_name(name)
