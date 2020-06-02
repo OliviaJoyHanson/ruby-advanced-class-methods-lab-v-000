d7673d230d7e76ce4b4991ce146b4dfc0aeb86dd
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-02 12:51:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,54 +18,9 @@ class Song
     song
   end
 
-  def self.new_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    song
-  end
-
-  def self.create_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    self.all << song
-    song
-  end
-
-  def self.find_by_name(song_name)
-    self.all.detect {|song| song.name == song_name}
-  end
-
-  def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)
-      self.find_by_name(song_name)
-    else
-      self.create_by_name(song_name)
-    end
-  end
-
-  def self.alphabetical
-    self.all.sort_by { |song| song.name}
-  end
-
-  def self.new_from_filename(filename)
-    song = self.new
-    filename_array = filename.chomp(".mp3").split(" - ")
-    song.artist_name = filename_array[0]
-    song.name = filename_array[1]
-    song
-  end
-
-  def self.create_from_filename(filename)
-    song = self.new
-    filename_array = filename.chomp(".mp3").split(" - ")
-    song.artist_name = filename_array[0]
-    song.name = filename_array[1]
-    self.all << song
-    song
-  end
-
-  def self.destroy_all
-    @@all = []
+  def self.new_by_name(name)
+    @name = name
+    @name = self.new.name
   end
 
 end
